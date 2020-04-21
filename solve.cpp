e813a69ae64e7dc953d9f8716d75f8ef0edc20c2
amulya <amulya2@pdx.edu>
2020-04-20 17:27:40 -0700

    initial commit with DFS

---
+++ solve.cpp
@@ -0,0 +1,206 @@
+#include "maze.h"
+#include "path.h"
+#include<queue>
+#include<vector>
+#include<list>
+#include<tuple>
+#include<utility>
+#include<iostream>
+#include<climits>
+#include<sstream>
+#include<stack>
+#include<unordered_set>
+using namespace std;
+
+path solve_dfs(Maze& m, int rows, int cols);
+path solve_bfs(Maze& m, int rows, int cols);
+path solve_dijkstra(Maze& m, int rows, int cols);
+path solve_tour(Maze& m, int rows, int cols);
+
+
+class MyHashFunction { 
+public: 
+    // id is returned as hash function 
+    size_t operator()(const point& p) const
+    { 
+        return (hash<int>()(p.first)) ^ (hash<int>()(p.second)); 
+    } 
+}; 
+
+class MyEqualFunction { 
+public: 
+    bool operator()(const point& p1, const point& p2) const
+    { 
+        return ((p1.first == p2.first) && (p1.second == p2.second));
+    } 
+};
+
+point getUnvisitedNeighbors(Maze& m ,unordered_set<point, MyHashFunction, MyEqualFunction>& visited, point currPosition)
+{
+    point newLocation = make_pair(-1,-1);
+    if(m.can_go(0,currPosition.first, currPosition.second) && visited.find(currPosition + moveIn(0)) == visited.end())
+        {
+            newLocation = currPosition + moveIn(0);
+        }
+    else if(m.can_go(1,currPosition.first, currPosition.second) && visited.find(currPosition + moveIn(1)) == visited.end())
+        {
+            newLocation = currPosition + moveIn(1);
+        }
+    else if(m.can_go(2,currPosition.first, currPosition.second) && visited.find(currPosition + moveIn(2)) == visited.end())
+        {
+            newLocation = currPosition + moveIn(2);
+        }
+    else if(m.can_go(3,currPosition.first, currPosition.second) && visited.find(currPosition + moveIn(3)) == visited.end())
+        {
+            newLocation = currPosition + moveIn(3); 
+        }
+    return newLocation;
+}
+
+int main(int argc, char** argv)
+{
+    if(argc != 4)
+    {
+        cerr << "usage:\n"
+             << "./maze option rows cols\n"
+             << " options:\n"
+             << "  -dfs: depth first search (backtracking)\n"
+             << "  -bfs: breadth first search\n"
+             << "  -dij: dijkstra's algorithm\n"
+             << "  -tour: all corners tour\n"
+             << "  -basic: run dfs, bfs, and dij\n"
+             << "  -advanced: run dfs, bfs, dij and tour" << endl;
+        return 0;
+    }
+    string opt(argv[1]);
+
+    int rows, cols;
+    stringstream s;
+    s << argv[2] << " " << argv[3];
+    s >> rows >> cols;
+
+    // construct a new random maze;
+    Maze m(rows, cols);
+
+    // print the initial maze out
+    cout << "Initial maze" << endl;
+    m.print_maze(cout, opt == "-dij" || opt == "-tour");
+
+    if(opt == "-dfs")
+    {
+        cout << "\nSolved dfs" << endl;
+        path p = solve_dfs(m, rows, cols);
+        m.print_maze_with_path(cout, p, false, false);
+    }
+
+
+    if(opt == "-bfs")
+    {
+        cout << "\nSolved bfs" << endl;
+        path p = solve_bfs(m, rows, cols);
+        m.print_maze_with_path(cout, p, false, false);
+    }
+
+    if(opt == "-dij")
+    {
+        cout << "\nSolved dijkstra" << endl;
+        path p = solve_dijkstra(m, rows, cols);
+        m.print_maze_with_path(cout, p, true, false);
+    }
+
+    if(opt == "-tour")
+    {
+        cout << "\nSolved all courners tour" << endl;
+        path p = solve_tour(m, rows, cols);
+        m.print_maze_with_path(cout, p, true, true);
+    }
+    if(opt == "-basic")
+    {
+        cout << "\nSolved dfs" << endl;
+        path p = solve_dfs(m, rows, cols);
+        m.print_maze_with_path(cout, p, false, false);
+
+        cout << "\nSolved bfs" << endl;
+        p = solve_bfs(m, rows, cols);
+        m.print_maze_with_path(cout, p, false, false);
+
+        cout << "\nSolved dijkstra" << endl;
+        p = solve_dijkstra(m, rows, cols);
+        m.print_maze_with_path(cout, p, true, false);
+    }
+    if(opt == "-advanced")
+    {
+        cout << "\nSolved dfs" << endl;
+        path p = solve_dfs(m, rows, cols);
+        m.print_maze_with_path(cout, p, false, false);
+
+        cout << "\nSolved bfs" << endl;
+        p = solve_bfs(m, rows, cols);
+        m.print_maze_with_path(cout, p, false, false);
+
+        cout << "\nSolved dijkstra" << endl;
+        p = solve_dijkstra(m, rows, cols);
+        m.print_maze_with_path(cout, p, true, false);
+
+        cout << "\nSolved all courners tour" << endl;
+        p = solve_tour(m, rows, cols);
+        m.print_maze_with_path(cout, p, true, true);
+    }
+}
+
+path solve_dfs(Maze& m, int rows, int cols)
+{
+
+    stack<point> stacklist;
+    point current = make_pair(0,0);
+    stacklist.push(current);
+
+    int row = m.rows();
+    int col = m.columns();
+    point currPosition;
+    point end = make_pair(row-1,col-1);
+    bool flag = false;
+
+    unordered_set<point, MyHashFunction, MyEqualFunction> visited;
+    list<point> path;
+
+    while(true)
+    {
+        currPosition = stacklist.top();
+        if(currPosition == end)
+            break;
+        point newPosition;
+
+        if(visited.find(currPosition) == visited.end())
+            visited.emplace(currPosition);
+
+        newPosition = getUnvisitedNeighbors(m,visited,currPosition);
+
+        if(newPosition != make_pair(-1,-1))
+        {
+            stacklist.push(newPosition);
+            path.push_back(newPosition);
+        }
+        else{
+            stacklist.pop();
+            path.pop_back();
+        }
+    }
+    return path;
+}
+
+path solve_bfs(Maze& m, int rows, int cols)
+{
+    
+    return list<point>();
+}
+
+path solve_dijkstra(Maze& m, int rows, int cols)
+{
+    return list<point>();
+}
+
+path solve_tour(Maze& m, int rows, int cols)
+{
+    return list<point>();
+}
